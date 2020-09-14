1258ea87a5c8e60aed2054735e41c0b493f28874
Jim Dowling <jdowling@sics.se>
2020-09-14 16:41:54 +0200

    update to apache v2 license

--- sign.sh
+++ sign.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ../pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
-
+VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ./pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
+echo "Version is $VERSION"
 gpg2 -ab target/hopsworks-cli-${VERSION}-shaded.jar
 gpg2 --verify target/hopsworks-cli-${VERSION}-shaded.jar.asc
 
